0755827998ab36493660c7b93c4fa3bdec4fcc38
orkEnable <ork.develop@gmail.com>
2025-11-11 07:01:32 +0900

    fix:coderabbit指摘修正

--- backend/src/github.strategy.ts
+++ backend/src/github.strategy.ts
@@ -7,9 +7,9 @@ import {Strategy} from 'passport-github2';
 export class GithubStrategy extends PassportStrategy (Strategy, 'github'){
   constructor(private configService: ConfigService) {
     super({
-      clientID: configService.get<string>('GITHUB_CLIENT_ID'),
-      clientSecret: configService.get<string>('GITHUB_CLIENT_SECRET'),
-      callbackURL: configService.get<string>('GITHUB_CALLBACK_URL'),
+      clientID: configService.getOrThrow<string>('GITHUB_CLIENT_ID'),
+      clientSecret: configService.getOrThrow<string>('GITHUB_CLIENT_SECRET'),
+      callbackURL: configService.getOrThrow<string>('GITHUB_CALLBACK_URL'),
       scope: ['user', 'repo'],
     });
   }
