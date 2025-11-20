ee350842025798bd9dab33ee3eaafe7d189d3f91
MIYAZAKI\zaki7 <zaki725725725@gmail.com>
2025-11-20 20:29:18 +0900

    Revert "Merge pull request #1 from hagio-gakuto/feature/user-detail"
    This reverts commit f12af6a6118a9f3a8a2194f8b9d53f6f6f52eaab, reversing
    changes made to 967db1fdb138449c34b1873981bad28d8be26d58.

--- backend/prisma/migrations/20251102072835_add_monster_image_url/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Monster" ADD COLUMN     "imageUrl" TEXT;
--- backend/src/application/user/dto/user-response.dto.ts
+++
@@ -1,9 +0,0 @@
-export type UserResponseDto = {
-  id: string;
-  name: string;
-  age: number;
-  hobby: string;
-  imageUrl: string | null;
-  introduction: string | null;
-};
-
